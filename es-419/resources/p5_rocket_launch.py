5bf4ecece11b97a4ec099774bd0eef1c68b751a7
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-03-07 10:53:08 +0000

    New translations p5_rocket_launch.py (Spanish, Latin America)

--- es-419/resources/p5_rocket_launch.py
+++ es-419/resources/p5_rocket_launch.py
@@ -1,8 +1,8 @@
-# Definitions for compatibility with the p5py processing library 
+# Definiciones de compatibilidad con la biblioteca de procesamiento p5py 
 from processing import *
 import __main__
 
-# Shape
+# Forma
 from processing import rectMode as rect_mode
 from processing import ellipseMode as ellipse_mode
 from processing import strokeWeight as stroke_weight
@@ -11,36 +11,36 @@ from processing import strokeJoin as stroke_join
 from processing import noStroke as no_stroke
 from processing import noFill as no_fill
 
-# Fonts
+# Fuentes
 from processing import createFont as create_font
 from processing import loadFont as load_font
 from processing import textFont as text_font
 
-# Text
+# Texto
 from processing import textAlign as text_align
 from processing import textLeading as text_leading
 from processing import textMode as text_mode
 from processing import textSize as text_size
 from processing import textWidth as text_width
 
-# Colour
+# Color
 from processing import blendColor as blend_color
 from processing import lerpColor as lerp_color
 from processing import color as Color
   
-# Images
+# Imágenes
 from processing import createImage as create_image
 from processing import imageMode as image_mode
 from processing import loadImage as load_image
 from processing import noTint as no_tint
 from processing import requestImage as request_image
 
-# Environment
+# Ambiente
 from processing import frameRate as frame_rate
 from processing import noCursor as no_cursor
 from processing import noLoop as no_loop
 
-# Transform
+# Transformar
 from processing import applyMatrix as apply_matrix
 from processing import popMatrix as pop_matrix
 from processing import printMatrix as print_matrix
@@ -53,10 +53,10 @@ from processing import popStyle as pop_style
 
 from processing import run as main_run
 
-# Keyboard
+# Teclado
 
 def mousePressed():
-  if hasattr(__main__, "mouse_pressed"):
+  si hasattr(__main__, "mouse_pressed"):
     mouse_pressed = getattr(__main__, "mouse_pressed")
     mouse_pressed()
     
@@ -104,7 +104,7 @@ def grid():
   line(0, height/2, width, height/2)
   line(width/2, 0, width/2, height)
   x_coords = [0, width/2, width]
-  y_coords = [0, height/2, height]
+  y_coords = [0, width/2, width]
  
   for x in x_coords:
     for y in y_coords:
