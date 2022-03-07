b3a3719f2408abb2d294c72ddccb69c704c795c7
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-03-07 10:53:16 +0000

    New translations rocket_launch.py (Dutch)

--- nl-NL/resources/rocket_launch.py
+++ nl-NL/resources/rocket_launch.py
@@ -1,72 +1,72 @@
 #!/bin/python3
 
-# Import library code
+# Bibliotheekcode importeren
 from p5 import *
 from random import randint
 
-# Setup global variables
-screen_size = 400
-rocket_y = screen_size # start at the bottom
-burn = 100 # how much fuel is burned in each frame
-orbit_radius = 250
-orbit_y = screen_size - orbit_radius
+# Globale variabelen instellen
+scherm_grootte = 400
+raket_y = scherm_grootte # begin onderaan
+verbruik = 100 # hoeveel brandstof wordt er in elk frame verbrand
+omloopbaan_straal = 250
+omloopbaan_y = scherm_grootte - omloopbaan_straal
 
-# The draw_rocket function goes here
-def draw_rocket():
+# De teken_raket functie komt hier
+def teken_raket():
 
-  global rocket_y, fuel, burn
+  global raket_y, brandstof, verbruik
   
-  if fuel >= burn and rocket_y > orbit_y: # still flying
-    rocket_y -= 1 # move the rocket
-    fuel -= burn # burn fuel
-    print('Fuel left: ', fuel)
+  if brandstof >= verbruik and raket_y > omloopbaan_y: # vliegt nog steeds
+    raket_y -= 1 # verplaats de raket
+    brandstof -= verbruik # brandstof verbruik
+    print('Brandstof over: ', brandstof)
   
-    no_stroke() # Turn off the stroke
+    no_stroke() # Zet de lijn uit
   
-    for i in range(25): # draw 25 burning exhaust ellipses
-      fill(255, 255 - i*10, 0) # yellow
-      ellipse(width/2, rocket_y + i, 8, 3) # i increases each time the loop repeats
+    for i in range(25): # teken 25 brandende uitstoot ellipsen
+      fill(255, 255 - i*10, 0) # geel
+      ellipse(width/2, raket_y + i, 8, 3) # i neemt toe elke keer dat de lus wordt herhaald
     
-    fill(200, 200, 200, 100) # transparent grey
-    for i in range(20): # draw 20 random smoke ellipses
-      ellipse(width/2 + randint(-5, 5), rocket_y + randint(20, 50), randint(5, 10), randint(5, 10))
+    fill(200, 200, 200, 100) # transparant grijs
+    for i in range(20): # teken 20 willekeurige rook ellipsen
+      ellipse(width/2 + randint(-5, 5), raket_y + randint(20, 50), randint(5, 10), randint(5, 10))
   
-  if fuel < burn and rocket_y > orbit_y: # No more fuel and not in orbit
-    tint(255, 0, 0) # Failure
-  elif fuel < 1000 and rocket_y <= orbit_y:
-    tint(0, 255, 0) # Success
-  elif fuel >= 1000 and rocket_y <= orbit_y: 
-    tint(255, 200, 0) # Too much fuel
+  if brandstof < verbruik and raket_y > omloopbaan_y: # Geen brandstof meer en niet in een omloopbaan
+    tint(255, 0, 0) # Mislukt
+  elif brandstof < 1000 en raket_y <= omloopbaan_y:
+    tint(0, 255, 0) # Gelukt
+  elif brandstof >= 1000 en raket_y <= omloopbaan_y: 
+    tint(255, 200, 0) # Te veel brandstof
   
-  image(rocket, width/2, rocket_y, 64, 64)
+  image(raket, width/2, height/2, 64, 64)
   no_tint()
   
 
-# The draw_background function goes here
-def draw_background():
-  background(0) # short for background(0, 0, 0) - black 
-  image(planet, width/2, height, 300, 300) # draw the image
+# De functie teken_achtergrond komt hier
+def teken_achtergrond():
+  background(0) # afkorting voor background(0, 0, 0) - zwart 
+  image(planeet, width/2, height, 300, 300) # teken de afbeelding
   
-  no_fill() # Turn off any fill
-  stroke(255) # Set a white stroke
+  no_fill() # Zet elke vulling uit
+  stroke(255) # Stel een witte lijn in
   stroke_weight(2)
-  ellipse(width/2, height, orbit_radius*2, orbit_radius*2)
+  ellipse(width/2, height, omloopbaan_straal*2, omloopbaan_straal*2)
   
 
 def setup():
-  # Setup your animation here
-  size(screen_size, screen_size)
+  # Stel hier je animatie in
+  size(scherm_grootte, scherm_grootte)
   image_mode(CENTER)
-  global planet, rocket
-  planet = load_image('planet.png') # your chosen planet
-  rocket = load_image('rocket.png')
+  global planeet, raket
+  planeet = load_image('planet.png') # jouw gekozen planeet
+  raket = load_image('rocket.png')
 
 
 def draw():
-  # Things to do in every frame
-  draw_background()  
-  draw_rocket()
+  # Dingen om te doen in elk frame
+  teken_achtergrond()  
+  teken_raket()
   
 
-fuel = int(input('How many kilograms of fuel do you want to use?'))
+brandstof = int(input('Hoeveel kilogram brandstof wil je gebruiken?'))
 run()
