8968ffd1607a4fa1b52f65810db6a3286cd8ba91
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-02-21 12:34:34 +0000

    New translations main.py (Dutch)

--- nl-NL/code/rocket-launch-solution/main.py
+++ nl-NL/code/rocket-launch-solution/main.py
@@ -1,73 +1,73 @@
 #!/bin/python3
 
-# Import library code
+# Bibliotheekcode importeren
 from p5 import *
 from random import randint
 
-# Setup global variables
-screen_size = 400
-rocket_y = 400
-burn = 100
-orbit_radius = 250
-orbit_y = screen_size - orbit_radius
+# Globale variabelen instellen
+scherm_grootte = 400
+raket_y = 400
+branden = 100
+omloopbaan_straal = 250
+omloopbaan_y = scherm_grootte - omloopbaan_straal
 
 
-# The draw_rocket function goes here
-def draw_rocket():
-    global rocket_y, fuel, burn
+# De teken_raket functie komt hier
+def teken_raket():
+    global raket_y, brandstof, verbruik
 
-    if fuel >= burn and rocket_y > orbit_y:
-        rocket_y -= 1
-        fuel -= burn
-        print('Fuel left: ', fuel)
+    if brandstof >= verbruik and raket_y > omloopbaan_y:
+        raket_y = 1
+        brandstof -= verbruik
+        print('Brandstof over: ', brandstof)
 
         no_stroke()
 
         for i in range(25):
             fill(255, 255 - i * 10, 0)
-            ellipse(width/2, rocket_y + i, 8, 3)
+            ellipse(width/2, raket_y + i, 8, 3)
 
-        fill(200, 200, 200, 100)  # Transparent grey
-        for i in range(20):  # Draw 20 random smoke ellipses
-            ellipse(width/2 + randint(-5, 5), rocket_y +
+        fill(200, 200, 200, 100) # Transparant grijs
+        for i in range(20): # Teken 20 willekeurige rook ellipsen
+            ellipse(width/2 + randint(-5, 5), raket_y +
                     randint(20, 50), randint(5, 10), randint(5, 10))
 
-    if fuel < burn and rocket_y > orbit_y:
+    if brandstof < verbruik and raket_y > omloopbaan_y:
         tint(255, 0, 0)
-    elif fuel < 1000 and rocket_y <= orbit_y:
+    elif brandstof < 1000 en raket_y <= omloopbaan_y:
         tint(0, 255, 0)
-    elif fuel >= 1000 and rocket_y <= orbit_y:
+    elif brandstof >= 1000 en raket_y <= omloopbaan_y:
         tint(255, 200, 0)
 
-    image(rocket, width/2, rocket_y, 64, 64)
+    image(raket, width/2, height/2, 64, 64)
     no_tint()
 
 
-# The draw_background function goes here
-def draw_background():
+# De functie teken_achtergrond komt hier
+def teken_achtergrond():
     background(0)
-    image(planet, width/2, height, 300, 300)
+    image(planeet, width/2, height, 300, 300)
 
     no_fill()
     stroke(255)
     stroke_weight(2)
-    ellipse(width/2, height, orbit_radius * 2, orbit_radius * 2)
+    ellipse(width/2, height, omloopbaan_straal*2, omloopbaan_straal*2)
 
 
 def setup():
-    # Setup your animation here
-    size(screen_size, screen_size)
+    # Stel hier je animatie in
+    size(scherm_grootte, scherm_grootte)
     image_mode(CENTER)
-    global planet, rocket
-    planet = load_image('planet.png')
-    rocket = load_image('rocket.png')
+    global planeet, raket
+    planeet = load_image('planet.png')
+    raket = load_image('rocket.png')
 
 
 def draw():
-    # Things to do in every frame
-    draw_background()
-    draw_rocket()
+    # Dingen om te doen in elk frame
+    teken_achtergrond()
+    teken_raket()
 
 
-fuel = int(input('How many kilograms of fuel do you want to use?'))
+brandstof = int(input('Hoeveel kilogram brandstof wil je gebruiken?'))
 run()
