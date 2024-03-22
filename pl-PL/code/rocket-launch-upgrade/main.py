b96cec0c34a053c3dcf2da8d54d058433cef9b68
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-03-22 16:09:17 +0000

    New translations main.py (Polish)

--- pl-PL/code/rocket-launch-upgrade/main.py
+++ pl-PL/code/rocket-launch-upgrade/main.py
@@ -1,89 +1,89 @@
 #!/bin/python3
 
-# Import library code
+# Importuj kod biblioteki
 from p5 import *
 from random import randint
 
-# Setup global variables
+# Ustaw zmienne globalne
 screen_size = 400
-rocket_y = screen_size  # Start at the bottom
-burn = 100  # How much fuel is burned in each frame
-orbit_radius = 250
+Rocket_y = screen_size # Zacznij od dołu
+Spalanie = 100 # Ile paliwa jest spalane w każdej klatce
+orbita_promień = 250
 orbit_y = screen_size - orbit_radius
 high_orbit_radius = 350
 high_orbit_y = screen_size - high_orbit_radius
-speed = 1  # How far the rocket flies each frame
+Prędkość = 1 # jak daleko rakieta leci w każdej klatce
 
-# The draw_rocket function goes here
+# Funkcja draw_rocket pojawia się tutaj
 
 
 def draw_rocket():
-    global rocket_y, fuel, burn
+    global rocket_y, paliwo, spalanie
 
-    if fuel >= burn and rocket_y > high_orbit_y:  # Still flying
-        rocket_y -= speed  # Move the rocket
-        fuel -= burn  # Burn fuel
-        print('Fuel left: ', fuel)
+    IF fuel >= spal i ROCKET_y > high_orbit_y: # Nadal leci
+        Rocket_y -= prędkość # Przesuń rakietę
+        Paliwo -= spalanie # spalanie paliwa
+        Print('Pozostało paliwo: ', paliwo)
 
-        no_stroke()  # Turn off the stroke
+        No_stroke() # Wyłącz obrys
 
-        for i in range(25):  # Draw 25 burning exhaust ellipses
-            fill(255, 255 - i*10, 0)  # yellow
-            # i increases each time the loop repeats
-            ellipse(width/2, rocket_y + i, 8, 3)
+        For i in range(25): # Rysuj 25 palące się elipsy wydechowe
+            fill(255, 255 - i*10, 0) # żółty
+            # i zwiększa się za każdym razem, gdy pętla się powtarza
+            elipsa(width/2, rocket_y + i, 8, 3)
 
-        fill(200, 200, 200, 100)  # transparent grey
+        fill(200, 200, 200, 100) # przezroczysty szary
 
-        for i in range(20):  # draw 20 random smoke ellipses
-            ellipse(width/2 + randint(-5, 5), rocket_y +
+        for i in range(20): # narysuj 20 losowych elips dymu
+            elipsa(width/2 + randint(-5, 5), rocket_y +
                     randint(20, 50), randint(5, 10), randint(5, 10))
 
-    if fuel < burn and rocket_y > high_orbit_y:  # No more fuel and not in orbit
-        tint(255, 0, 0)  # Failure
-    elif rocket_y <= orbit_y and rocket_y > high_orbit_y:
-        tint(0, 255, 0)  # Success
-    elif fuel < 1000 and rocket_y <= high_orbit_y:
-        tint(0, 100, 200)  # High orbit success
-    elif fuel >= 1000 and rocket_y <= high_orbit_y:
-        tint(255, 200, 0)  # Too much fuel
+    IF paliwo < burn and ROCKET_y > high_orbit_y: # Brak paliwa i nie na orbicie
+        Tint(255, 0, 0) # Niepowodzenie
+    elif rocket_y <= orbita_y i rocket_y > high_orbit_y:
+        Tint(0, 255, 0) # sukces
+    paliwo elif < 1000 i rocket_y <= wysoka_orbita_y:
+        Tint(0, 100, 200) # sukces na wysokiej orbicie
+    paliwo elif >= 1000 i rocket_y <= wysoka_orbita_y:
+        Tint(255, 200, 0) # za dużo paliwa
 
-    image(rocket, width/2, rocket_y, 64, 64)
+    image(rakieta, width/2, rocket_y, 64, 64)
     no_tint()
 
 
-# The draw_background function goes here
+# Funkcja draw_background pojawia się tutaj
 def draw_background():
-    background(0)  # Short for background(0, 0, 0) - black
-    image(planet, width/2, height, 300, 300)  # draw the image
+    Background(0) # Krótkie dla tła(0, 0, 0) - czarny
+    image(planeta, szerokość/2, wysokość, 300, 300) # narysuj obraz
 
-    # Draw the lower orbit
-    no_fill()  # Turn off any fill
-    stroke(255)  # Set a white stroke
-    stroke_weight(2)
-    ellipse(width/2, height, orbit_radius*2, orbit_radius*2)
+    # Narysuj dolną orbitę
+    No_fill() # Wyłącz dowolne wypełnienie
+    Stroke(255) # Ustaw biały obrys
+    waga_uderzenia(2)
+    elipsa(width/2, height, orbit_radius*2, orbit_radius*2)
 
-    # Draw the higher orbit
-    stroke(0, 100, 200)  # Set a bluish stroke
-    stroke_weight(2)
-    ellipse(width/2, height, high_orbit_radius*2, high_orbit_radius*2)
+    # Narysuj wyższą orbitę
+    Stroke(0, 100, 200) # Ustaw niebieskawy skok
+    waga_uderzenia(2)
+    elipsa(width/2, height, high_orbit_radius*2, high_orbit_radius*2)
 
 
 def setup():
-    # Setup your animation here
-    size(screen_size, screen_size)
-    image_mode(CENTER)
-    global planet, rocket
-    planet = load_image('orange_planet.png')  # Your chosen planet
+    # Ustaw swoją animację tutaj
+    rozmiar(screen_size, screen_size)
+    Image_mode(ŚRODEK)
+    globalna planeta, rakieta
+    Planeta = load_image('orange_planet.png') # wybrana planeta
     rocket = load_image('rocket.png')
 
 
 def draw():
-    # Things to do in every frame
+    # Rzeczy do zrobienia w każdej klatce
     draw_background()
     draw_rocket()
 
 
-fuel = int(input('How many kilograms of fuel do you want to use?'))
-burn = int(input('How much fuel should the rocket burn each frame?'))
-speed = int(input('How far should the rocket travel each frame?'))
+Paliwo = int(input('Ile kilogramów paliwa chcesz użyć?'))
+Burn = int(input('Ile paliwa powinna spalić rakieta każda klatka?'))
+Prędkość = int(input("jak daleko rakieta powinna przejechać każdą klatkę?"))
 run()
