3bcee0547f17b847a428f000adc7e4e15ad33f9f
Laura Sach <5183697+lawsie@users.noreply.github.com>
2024-10-21 14:59:17 +0100

    Update starter code

--- en/code/rocket-launch-starter/main.py
+++ en/code/rocket-launch-starter/main.py
@@ -3,7 +3,7 @@ from p5 import *
 from random import randint
 
 # Set up global variables
-
+screen_size = 400
 
 # The draw_rocket function goes here
 
@@ -14,12 +14,16 @@ from random import randint
 
 
 def setup():
-  # Set up your animation here
-  
-
+    # Set up your animation here
+    size(screen_size, screen_size)
+    image_mode(CENTER)
+    global planet
+    planet = load_image('planet.png')
+   
 
 def draw():
-  # Things to do in every frame
+    # Things to do in every frame
+
     
 
 
