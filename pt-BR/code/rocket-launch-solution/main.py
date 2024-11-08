74c7bd8bb37ba3db1c7720bcd814e88cfcfe5fc2
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-11-08 13:59:40 +0000

    New translations main.py (Portuguese, Brazilian)

--- pt-BR/code/rocket-launch-solution/main.py
+++ pt-BR/code/rocket-launch-solution/main.py
@@ -1,66 +1,42 @@
-#!/bin/python3
-
 # Importar código da biblioteca
 from p5 import *
 from random import randint
 
-# Configurar variáveis globais
+# Set up global variables
 tamanho_da_tela = 400
-foguete_y = 400
-queimar = 100
-raio_orbita = 250
-orbita_y = tamanho_da_tela - raio_orbita
+rocket_position = screen_size
 
 
 # A função desenhar_foguete vai aqui
 def desenhar_foguete():
-    global foguete_y, combustível, queimar
-
-    if combustivel >= queimar and foguete_y > orbita_y:
-        foguete_y -= 1
-        combustível -= queimar
-        print('Combustível restante: ', combustivel)
-
-        no_stroke()
-
-        for i in range(25):
-            fill(255, 255 - i * 10, 0)
-            elipse (largura/2, foguete_y + i, 8, 3)
-
-        fill(200, 200, 200, 100) # Cinza transparente
-        for i in range(20): # Desenha 20 elipses de fumaça aleatórias
-            elipse(largura/2 + randint(-5, 5), foguete_y +
-                    randint(20, 50), randint(5, 10), randint(5, 10))
-
-    if combustível < queimar and foguete_y >orbita_y:
-        tint(255, 0, 0)
-    elif combustivel <1000 and foguete_y <= orbita_y:
-        tint(0, 255, 0)
-    elif combustivel >= 1000 and foguete_y <= orbita_y:
-        tint(255, 200, 0)
-
-    imagem(foguete, largura/2, foguete_y, 64, 64)
-    no_tint()
+    global rocket_position
+    rocket_position = rocket_position - 1
+    image(rocket, width / 2, rocket_position, 64, 64)
+    fill(200, 200, 200, 100)
+    no_stroke()
+    for i in range(20):
+        circle_size = randint(5, 10)
+        ellipse(
+            screen_size / 2 + randint(-5, 5),
+            rocket_position + randint(20, 50),
+            circle_size,
+            circle_size,
+        )
 
 
 # A função desenhar_fundo vai aqui
 def desenhar_plano_de_fundo():
-    background(0)
-    image(planeta, largura/2, altura, 300, 300)
-
-    no_fill()
-    stroke(255)
-    stroke_weight(2)
-    ellipse(largura/2, altura, raio_orbita * 2, raio_orbita * 2)
+    background(0, 0, 0)
+    image(planet, screen_size / 2, screen_size, 300, 300)
 
 
 def setup():
-    # Configure sua animação aqui
+    # Set up your animation here
     tamanho(tamanho_da_tela, tamanho_da_tela)
     modo_imagem(CENTRO)
     planeta global, foguete
-    planeta = carregar_imagem('planeta.png')
-    foguete = carregar_imagem('rocket.png')
+    planet = load_image("purple_planet.png")
+    rocket = load_image("rocket.png")
 
 
 def draw():
@@ -69,5 +45,4 @@ def draw():
     desenhar_foguete()
 
 
-combustivel = int(input('Quantos quilogramas de combustível você quer usar?'))
 run()
