728de6e35f0ab6ad397328447cba8d27d5765d9c
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-11-08 13:59:32 +0000

    New translations main.py (Italian)

--- it-IT/code/rocket-launch-solution/main.py
+++ it-IT/code/rocket-launch-solution/main.py
@@ -1,66 +1,42 @@
-#!/bin/python3
-
 # Importa il codice della libreria
 from p5 import *
 from random import randint
 
-# Imposta le variabili globali
+# Set up global variables
 screen_size = 400
-rocket_y = 400
-burn = 100
-orbit_radius = 250
-orbit_y = screen_size - orbit_radius
+rocket_position = screen_size
 
 
 # La funzione draw_rocket va qui
 def draw_rocket():
-    global rocket_y, fuel, burn
-
-    if fuel >= burn and rocket_y > orbit_y:
-        rocket_y -= 1
-        fuel -= burn
-        print('Carburante rimanente: ', fuel)
-
-        no_stroke()
-
-        for i in range(25):
-            fill(255, 255 - i * 10, 0)
-            ellipse(width/2, rocket_y + i, 8, 3)
-
-        fill(200, 200, 200, 100) # Grigio trasparente
-        for i in range(20): # Disegna 20 ellissi di fumo casuali
-            ellipse(width/2 + randint(-5, 5), rocket_y +
-                    randint(20, 50), randint(5, 10), randint(5, 10))
-
-    if fuel < burn and rocket_y > orbit_y:
-        tint(255, 0, 0)
-    elif fuel < 1000 and rocket_y <= orbit_y:
-        tint(0, 255, 0)
-    elif fuel >= 1000 and rocket_y <= orbit_y:
-        tint(255, 200, 0)
-
-    image(rocket, width/2, rocket_y, 64, 64)
-    no_tint()
+    global rocket_position
+    rocket_position = rocket_position - 1
+    image(rocket, width / 2, rocket_position, 64, 64)
+    fill(200, 200, 200, 100)
+    no_stroke()
+    for i in range(20):
+        circle_size = randint(5, 10)
+        ellipse(
+            screen_size / 2 + randint(-5, 5),
+            rocket_position + randint(20, 50),
+            circle_size,
+            circle_size,
+        )
 
 
 # La funzione draw_ background va qui
 def draw_background():
-    background(0)
-    image(planet, width/2, height, 300, 300)
-
-    no_fill()
-    stroke(255)
-    stroke_weight(2)
-    ellipse(width/2, height, orbit_radius * 2, orbit_radius * 2)
+    background(0, 0, 0)
+    image(planet, screen_size / 2, screen_size, 300, 300)
 
 
 def setup():
-    # Imposta la tua animazione qui
+    # Set up your animation here
     size(screen_size, screen_size)
     image_mode(CENTER)
     global planet, rocket
-    planet = load_image('planet.png')
-    rocket = load_image('rocket.png')
+    planet = load_image("purple_planet.png")
+    rocket = load_image("rocket.png")
 
 
 def draw():
@@ -69,5 +45,4 @@ def draw():
     draw_rocket()
 
 
-fuel = int(input('Quanti chilogrammi di carburante vuoi usare?'))
 run()
