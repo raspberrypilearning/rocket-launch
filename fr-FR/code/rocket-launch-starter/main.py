9a808847f5545f5eca6bf64e5fe84b5113bd3259
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-11-08 13:59:58 +0000

    New translations main.py (French)

--- fr-FR/code/rocket-launch-starter/main.py
+++ fr-FR/code/rocket-launch-starter/main.py
@@ -1,11 +1,9 @@
-#!/bin/python3
-
 # Importer le code de la bibliothèque
 from p5 import *
 from random import randint
 
-# Configuration des variables globales
-
+# Set up global variables
+screen_size = 400
 
 # La fonction dessine_fusee vient ici
 
@@ -14,15 +12,21 @@ from random import randint
 # La fonction dessine_arriere_plan vient ici
 
 
-
+ 
 def setup():
-  # Configure ton animation ici
-  
-
+    # Set up your animation here
+    size(screen_size, screen_size)
+    image_mode(CENTER)
+    global planet
+    planet = load_image('planet.png')
+   
 
 def draw():
-  # Choses à faire dans chaque image
+    # Choses à faire dans chaque image
+
     
 
 
 run()
+ 
+ 
