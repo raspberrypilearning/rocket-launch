be696393d7397b6935727dbb0486ed134f4d2323
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-11-08 13:59:30 +0000

    New translations main.py (German)

--- de-DE/code/rocket-launch-solution/main.py
+++ de-DE/code/rocket-launch-solution/main.py
@@ -1,66 +1,42 @@
-#!/bin/python3
-
 # Bibliothekscode importieren
 from p5 import *
 from random import randint
 
-# Globale Variablen einrichten
+# Set up global variables
 bildschirm_groesse = 400
-rakete_y = 400
-verbrennen = 100
-orbit_radius = 250
-orbit_y = bildschirm_groesse - orbit_radius
+rocket_position = screen_size
 
 
 # Die Funktion „zeichne_rakete“ kommt hierher
 def zeichne_rakete():
-    global rakete_y, treibstoff, verbrennen
-
-    if treibstoff >= verbrennen and rakete_y > orbit_y:
-        rakete_y -= 1
-        treibstoff -= verbrennen
-        print('Verbleibender Treibstoff: ', treibstoff)
-
-        no_stroke()
-
-        for i in range(25):
-            fill(255, 255 - i * 10, 0)
-            ellipse(width/2, rakete_y + i, 8, 3)
-
-        fill(200, 200, 200, 100) # Transparent grau
-        for i in range(20): # Zeichne 20 zufällige Rauchellipsen
-            ellipse(width/2 + randint(-5, 5), rakete_y +
-                    randint(20, 50), randint(5, 10), randint(5, 10))
-
-    if treibstoff < verbrennen and rakete_y > orbit_y:
-        tint(255, 0, 0)
-    elif treibstoff < 1000 and rakete_y <= orbit_y:
-        tint(0, 255, 0)
-    elif treibstoff >= 1000 and rakete_y <= orbit_y:
-        tint(255, 200, 0)
-
-    image(rakete, width/2, rakete_y, 64, 64)
-    no_tint()
+    global rocket_position
+    rocket_position = rocket_position - 1
+    image(rocket, width / 2, rocket_position, 64, 64)
+    fill(200, 200, 200, 100)
+    no_stroke()
+    for i in range(20):
+        circle_size = randint(5, 10)
+        ellipse(
+            screen_size / 2 + randint(-5, 5),
+            rocket_position + randint(20, 50),
+            circle_size,
+            circle_size,
+        )
 
 
 # Die Funktion „zeichne_hintergrund“ kommt hierher
 def zeichne_hintergrund():
-    background(0)
-    image(planet, width/2, height, 300, 300)
-
-    no_fill()
-    stroke(255)
-    stroke_weight(2)
-    ellipse(width/2, height, orbit_radius * 2, orbit_radius * 2)
+    background(0, 0, 0)
+    image(planet, screen_size / 2, screen_size, 300, 300)
 
 
 def setup():
-    # Richte hier Deine Animation ein
+    # Set up your animation here
     size(bildschirm_groesse, bildschirm_groesse)
     image_mode(CENTER)
     global planet, rakete
-    planet = load_image('planet.png')
-    rakete = load_image('rocket.png')
+    planet = load_image("purple_planet.png")
+    rocket = load_image("rocket.png")
 
 
 def draw():
@@ -69,5 +45,4 @@ def draw():
     zeichne_rakete()
 
 
-treibstoff = int(input('Wie viele Kilogramm Treibstoff willst Du verbrauchen?'))
 run()
