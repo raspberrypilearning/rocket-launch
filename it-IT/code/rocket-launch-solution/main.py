6c18051ad0bb255a461305ce83fc7f575524475a
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-12-02 15:57:37 +0000

    New translations main.py (Italian)

--- it-IT/code/rocket-launch-solution/main.py
+++ it-IT/code/rocket-launch-solution/main.py
@@ -2,23 +2,23 @@
 from p5 import *
 from random import randint
 
-# Set up global variables
+# Imposta le variabili globali
 screen_size = 400
-rocket_position = screen_size
+orbit_y = screen_size - orbit_radius
 
 
 # La funzione draw_rocket va qui
 def draw_rocket():
-    global rocket_position
-    rocket_position = rocket_position - 1
-    image(rocket, width / 2, rocket_position, 64, 64)
-    fill(200, 200, 200, 100)
+    global rocket_y, fuel, burn
+    rocket_y -= 1
+    image(rocket, width/2, rocket_y, 64, 64)
+    fill(200, 200, 200, 100) # Grigio trasparente
     no_stroke()
-    for i in range(20):
-        circle_size = randint(5, 10)
-        ellipse(
-            screen_size / 2 + randint(-5, 5),
-            rocket_position + randint(20, 50),
+    for i in range(25):
+        fill(255, 255 - i * 10, 0)
+        ellipse(width/2, rocket_y + i, 8, 3)
+            ellipse(width/2 + randint(-5, 5), rocket_y +
+            randint(20, 50), randint(5, 10), randint(5, 10))
             circle_size,
             circle_size,
         )
@@ -26,17 +26,17 @@ def draw_rocket():
 
 # La funzione draw_ background va qui
 def draw_background():
-    background(0, 0, 0)
-    image(planet, screen_size / 2, screen_size, 300, 300)
+    background(0)
+    image(planet, width/2, height, 300, 300)
 
 
 def setup():
-    # Set up your animation here
+    # Imposta la tua animazione qui
     size(screen_size, screen_size)
     image_mode(CENTER)
     global planet, rocket
-    planet = load_image("purple_planet.png")
-    rocket = load_image("rocket.png")
+    planet = load_image('planet.png')
+    rocket = load_image('rocket.png')
 
 
 def draw():
