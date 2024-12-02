2e5ba3ff45c05aec342262b93ffae8eeebdb63ac
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-12-02 15:58:04 +0000

    New translations main.py (Italian)

--- it-IT/code/rocket-launch-starter/main.py
+++ it-IT/code/rocket-launch-starter/main.py
@@ -2,7 +2,7 @@
 from p5 import *
 from random import randint
 
-# Set up global variables
+# Imposta le variabili globali
 screen_size = 400
 
 # La funzione draw_rocket va qui
@@ -14,7 +14,7 @@ screen_size = 400
 
  
 def setup():
-    # Set up your animation here
+    # Imposta la tua animazione qui
     size(screen_size, screen_size)
     image_mode(CENTER)
     global planet
